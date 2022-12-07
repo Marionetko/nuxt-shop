a140379d8aecbca654af8097f6b2e55edaf05e9b
Marionetko <webius.net@gmail.com>
2022-12-07 18:27:12 +0200

    added pages folder with some pages in it

--- app.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div>
-    <p>Hello, Nuxt!</p>
-  </div>
-</template>
---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h2>Home</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum aut labore magnam, nam pariatur laudantium! Assumenda, expedita maxime! Reprehenderit voluptatum aspernatur ipsa excepturi nulla corrupti nostrum necessitatibus suscipit, sequi sint.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum aut labore magnam, nam pariatur laudantium! Assumenda, expedita maxime! Reprehenderit voluptatum aspernatur ipsa excepturi nulla corrupti nostrum necessitatibus suscipit, sequi sint.</p>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+  h2 {
+    margin-bottom: 20px;
+    font-size: 36px;
+  }
+  p {
+    margin: 20px 0;
+  }
+</style>
