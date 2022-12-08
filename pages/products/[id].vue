287d8e83861998cbf95151fb89c30371cdfc913e
Marionetko <webius.net@gmail.com>
2022-12-08 15:50:42 +0200

    added dynamical route parameter in [id].vue

---
+++ pages/products/[id].vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <p>Product details for {{ id }}</p>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nobis nesciunt sint quisquam necessitatibus ducimus, mollitia quibusdam quidem iusto totam voluptas.</p>
+  </div>
+</template>
+
+<script setup>
+  const { id } = useRoute().params
+</script>
+
+<style scoped>
+
+</style>
