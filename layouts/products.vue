4812aca6a64fd8d5a13f5c3939d2b643d88e8ee5
Marionetko <webius.net@gmail.com>
2022-12-08 16:41:39 +0200

    added layout to products.vue file

---
+++ layouts/products.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <header>
+      <nav>
+        <NuxtLink to="/products">Nuxt Shop Merch</NuxtLink>
+      </nav>
+    </header>
+
+    <!-- output the page content -->
+    <div>
+      <slot />
+    </div>
+
+    <footer>
+      <ul>
+        <li>
+          <NuxtLink to="/">Home</NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/about">About</NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/products">Products</NuxtLink>
+        </li>
+      </ul>
+    </footer>
+  </div>
+</template>
+
+<style scoped>
+ .router-link-exact-active {
+    color: #e4eff0;
+ }
+</style>
