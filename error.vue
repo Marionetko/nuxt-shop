3461615f863a68ce34164451eb63ee8fc4e9f347
Marionetko <webius.net@gmail.com>
2022-12-08 21:10:32 +0200

    fixed clearError script

--- error.vue
+++ error.vue
@@ -3,14 +3,14 @@
     <p class="mt-7 text-7xl font-bold">{{ error.statusCode }}</p>
     <p class="mt-7 text-6xl">Ooops...</p>
     <p class="mt-7">{{ error.message }}</p>
-    <button class="btn my-7" @click="clearError">Go Home...</button>
+    <button class="btn my-7" @click="handleClearError">Go Home...</button>
   </div>
 </template>
 
 <script setup>
   defineProps(['error'])
 
-  const clearError = () => clearError({ redirect: '/' })
+  const handleClearError = () => clearError({ redirect: '/' })
 </script>
 
 <style scoped>
