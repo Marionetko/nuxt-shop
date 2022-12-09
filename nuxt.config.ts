d2623b11f9e10b888775c3121d8990e3920f90e6
Marionetko <webius.net@gmail.com>
2022-12-09 15:45:44 +0200

    added Metadata to nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxtjs/tailwindcss']
+  modules: ['@nuxtjs/tailwindcss'],
+  app: {
+    head: {
+      title: 'Nuxt Shop',
+      meta: [
+        { name: 'description', content: 'About Nuxt.js' }
+      ]
+    }
+  }
 })
