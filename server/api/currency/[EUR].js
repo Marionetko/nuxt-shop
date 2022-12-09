1540e383439bc4468147ca9675377b4c9613f055
Marionetko <webius.net@gmail.com>
2022-12-09 18:10:07 +0200

    customized Currence API

--- server/api/currency/[EUR].js
+++ server/api/currency/[EUR].js
@@ -2,4 +2,6 @@ export default defineEventHandler(async (event) => {
 
   const { EUR } = event.context.params
 
+  const uri = `https://api.currencyapi.com/v3/latest?currencies=${EUR}&apikey=`
+
 })
