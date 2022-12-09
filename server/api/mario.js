ff059591907853e07666c40edd9609f57b7fa5b2
Marionetko <webius.net@gmail.com>
2022-12-09 17:30:25 +0200

    added post data

--- server/api/mario.js
+++ server/api/mario.js
@@ -1,10 +1,13 @@
-export default defineEventHandler((event) => {
+export default defineEventHandler(async (event) => {
 
   // query params
   const { name } = getQuery(event)
 
+  // post data
+  const { age } = await readBody(event) 
+
   return {
-    message: `Hello ${name}!`
+    message: `Hello ${name}! You are ${age} years old.`
   }
 
 })
