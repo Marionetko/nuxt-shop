97bf822bb96498619a7b01923962b50ef35da92c
Marionetko <webius.net@gmail.com>
2022-12-09 16:53:48 +0200

    implemented custom server route

--- pages/about.vue
+++ pages/about.vue
@@ -3,11 +3,12 @@
     <h2>About</h2>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum aut labore magnam, nam pariatur laudantium! Assumenda, expedita maxime! Reprehenderit voluptatum aspernatur ipsa excepturi nulla corrupti nostrum necessitatibus suscipit, sequi sint.</p>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum aut labore magnam, nam pariatur laudantium! Assumenda, expedita maxime! Reprehenderit voluptatum aspernatur ipsa excepturi nulla corrupti nostrum necessitatibus suscipit, sequi sint.</p>
+    <div>{{ data }}</div>
   </div>
 </template>
 
 <script setup>
-
+  const { data } = await useFetch('/api/mario')
 </script>
 
 <style scoped>
